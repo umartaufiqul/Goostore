34281428cca0129007c929b8468b908b28d86736
Hyunseung Lim <charlie9807@kaist.ac.kr>
2019-12-01 15:51:42 +0900

    Hello3

--- app/src/main/java/com/example/goostore/MainPage.java
+++ app/src/main/java/com/example/goostore/MainPage.java
@@ -1,13 +1,28 @@
 package com.example.goostore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainPage extends AppCompatActivity {
+
+    ImageView profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        profileButton = findViewById(R.id.profilebtn);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPage.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
