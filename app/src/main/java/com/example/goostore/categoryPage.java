282756d8b7f0e54b8176a3b062591a2ade392028
Hyunseung Lim <charlie9807@kaist.ac.kr>
2019-12-08 05:12:04 +0900

    good2

---
+++ app/src/main/java/com/example/goostore/categoryPage.java
@@ -0,0 +1,14 @@
+package com.example.goostore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class categoryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_page);
+    }
+}
